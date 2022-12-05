f064fcf98e52b40b68f9c48b3a9c3dc76fb2e3b3
enagandulasnehapriya <116548367+enagandulasnehapriya@users.noreply.github.com>
2022-12-05 22:45:46 +0530

    Add files via upload

---
+++ greatest_of_2_nos.py
@@ -0,0 +1,7 @@
+#program to find the greatest of any given two numbers using if...else
+a=int(input('Enter a value'))
+b=int(input("Enter b value"))
+if(a>b):
+    print(a,"is greater")
+else:
+    print(b,"is greater")
